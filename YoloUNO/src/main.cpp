d4a674e5d49b8163c766a71881a3f6449571767e
longnguyencbct <clongnguynvn@gmail.com>
2025-02-17 19:18:42 +0700

    init Temperature & Humidity Monitoring

--- YoloUNO/src/main.cpp
+++ YoloUNO/src/main.cpp
@@ -1,6 +1,6 @@
-// Blinky LED
-
 #include <Arduino.h>
+#include <DHT20.h>
+
 
 void TaskLEDControl(void *pvParameters) {
   pinMode(GPIO_NUM_48, OUTPUT); // Initialize LED pin
@@ -13,9 +13,26 @@ void TaskLEDControl(void *pvParameters) {
       digitalWrite(GPIO_NUM_48, LOW); // Turn OFF LED
     }
     ledState = 1 - ledState;
-    Serial.println("LED State: " + String(ledState));
     vTaskDelay(2000);
   }
+}
+
+void TaskTemperature_Humidity(void *pvParameters){
+  DHT20 dht20;
+  Wire.begin(GPIO_NUM_11, GPIO_NUM_12);
+  dht20.begin();
+  while(1){
+    dht20.read();
+    double temperature = dht20.getTemperature();
+    double humidity = dht20.getHumidity();
+
+    Serial.print("Temp: "); Serial.print(temperature); Serial.print(" *C ");
+    Serial.print(" Humidity: "); Serial.print(humidity); Serial.print(" %");
+    Serial.println();
+    
+    vTaskDelay(5000);
+  }
+
 }
 
 
@@ -23,9 +40,10 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   xTaskCreate(TaskLEDControl, "LED Control", 2048, NULL, 2, NULL);
+  xTaskCreate(TaskTemperature_Humidity, "LED Control", 2048, NULL, 2, NULL);
+  
 }
 
 void loop() {
-  // Serial.println("Hello Custom Board");
-  // delay(1000);
+  
 }
