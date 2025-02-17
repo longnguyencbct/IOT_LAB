419b96393578195d7610d23ac02d5d9b53cd3176
longnguyencbct <clongnguynvn@gmail.com>
2025-02-17 18:42:34 +0700

    Added YoloUNO workspace

--- DOIT_ESP32_DEVKIT_V1/src/main.cpp
+++ DOIT_ESP32_DEVKIT_V1/src/main.cpp
@@ -1,18 +1,28 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+void TaskLEDControl(void *pvParameters) {
+  pinMode(GPIO_NUM_13, OUTPUT); // Initialize LED pin
+  int ledState = 0;
+  while(1) {
+    
+    if (ledState == 0) {
+      digitalWrite(GPIO_NUM_13, HIGH); // Turn ON LED
+    } else {
+      digitalWrite(GPIO_NUM_13, LOW); // Turn OFF LED
+    }
+    ledState = 1 - ledState;
+    vTaskDelay(2000);
+  }
+}
+
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  xTaskCreate(TaskLEDControl, "LED Control", 2048, NULL, 2, NULL);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Serial.println("Hello Custom Board");
+  // delay(1000);
 }
